81178552ccf4a04eebbb6def8ecae4ef820a7464
pfg <omegariisthebest@gmail.com>
2020-05-03 18:32:42 -0700

    update dependency downloader with fixed large paragraph support and use a commit before a breaking change in tree-sitter

--- deps/download.sh
+++ deps/download.sh
@@ -7,7 +7,11 @@ mkdir deps/build
 
 cd deps/build
     git clone https://github.com/tree-sitter/tree-sitter.git
-	git clone https://github.com/ikatyang/tree-sitter-markdown.git
+    cd tree-sitter
+        # before breaking change
+        git reset --hard c393591e1db759c0f16f877165a6370d06b22472
+    cd ..
+	git clone https://github.com/pfgithub/tree-sitter-markdown.git
 	git clone https://github.com/tree-sitter/tree-sitter-json.git
     cd tree-sitter-markdown/src
 		echo "Building markdown scanner..."
