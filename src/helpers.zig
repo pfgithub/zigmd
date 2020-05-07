9eb8c72f19a45ccb4f4f05b164c4819e84cdb088
pfg <omegariisthebest@gmail.com>
2020-05-07 13:43:14 -0700

    ///

--- src/helpers.zig
+++ src/helpers.zig
@@ -2,12 +2,20 @@ const std = @import("std");
 
 // these might be in the standard library already but I don't know how to describe what they do well enough to ask.
 // nice to have things
+
+/// A "struct" with enum keys
 pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
     const fields = @typeInfo(Enum).Enum.fields;
     const fieldCount = fields.len;
     return struct {
         const V = @This();
         data: [fieldCount]Value,
+
+        /// Example:
+        /// ```zig
+        /// EnumArray(enum{One, Two}, boolean)
+        ///   .init(.{.One = true, .Two = false});
+        /// ```
         pub fn init(values: var) V {
             const ValuesType = @TypeOf(values);
             const valuesInfo = @typeInfo(ValuesType).Struct;
@@ -40,6 +48,12 @@ pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
             }
             return .{ .data = res };
         }
+
+        /// Example:
+        /// ```zig
+        /// EnumArray(enum{One, Two}, boolean)
+        ///   .initDefault(false);
+        /// ```
         pub fn initDefault(value: Value) V {
             var res: [fieldCount]Value = undefined;
             inline for (fields) |field, i| {
@@ -47,22 +61,43 @@ pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
             }
             return .{ .data = res };
         }
-        pub fn get(arr: V, key: var) Value {
+
+        /// Example:
+        /// ```zig
+        /// var enumArr: EnumArray(enum{One, Two}, boolean)
+        /// enumArr.get(.One);
+        /// enumArr.get(.Two);
+        /// ```
+        pub fn get(arr: V, key: Enum) Value {
             return arr.data[@enumToInt(key)];
         }
-        pub fn getPtr(arr: *V, key: var) *Value {
+
+        /// gets a mutable pointer
+        pub fn getPtr(arr: *V, key: Enum) *Value {
             return &arr.data[@enumToInt(key)];
         }
-        pub fn set(arr: *V, key: var, value: Value) Value {
+        /// sets the value and returns the previous value
+        pub fn set(arr: *V, key: Enum, value: Value) Value {
             var prev = arr.data[@enumToInt(key)];
             arr.data[@enumToInt(key)] = value;
             return prev;
         }
     };
 }
+
 // holds all the values of the union at once instead of just one value
 pub fn UnionArray(comptime Union: T) type {
+    const tinfo = @typeInfo(Union);
+    const Enum = tinfo.Union.tag_type;
     return struct {
-        data: [@typeInfo(Enum).Enum.fields.len]Union,
+        const V = @This();
+        data: [fieldCount]Value,
+        /// UnionArray(union{a: i32, b: SomeEnum})
+        /// .init(.{.a = 25, .b = enumvalue})
+        /// so internally they are stored as union values
+        /// but it doesn't look like it
+        pub fn init(values: var) V {
+            // needs to assemble the object
+        }
     };
 }
