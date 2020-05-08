dc04e0c35a7aadd07b7404b84f6209138360e78f
pfg <omegariisthebest@gmail.com>
2020-05-07 20:02:11 -0700

    union array helper (and tests)

--- src/helpers.zig
+++ src/helpers.zig
@@ -3,6 +3,34 @@ const std = @import("std");
 // these might be in the standard library already but I don't know how to describe what they do well enough to ask.
 // nice to have things
 
+fn ensureAllDefined(comptime ValuesType: type, comptime Enum: type) void {
+    comptime {
+        const fields = @typeInfo(Enum).Enum.fields;
+        const fieldCount = fields.len;
+        const valuesInfo = @typeInfo(ValuesType).Struct;
+
+        var ensure: [fieldCount]bool = undefined;
+        for (fields) |field, i| {
+            ensure[i] = false;
+        }
+        for (valuesInfo.fields) |field| {
+            // var val: Enum = std.meta.stringToEnum(Enum, field.name);
+            var val: Enum = @field(Enum, field.name);
+            const index = @enumToInt(val);
+            if (ensure[index]) {
+                @compileError("Duplicate key: " ++ field.name);
+            }
+            ensure[index] = true;
+        }
+        for (fields) |field, i| {
+            if (!ensure[i]) {
+                @compileLog(@intToEnum(Enum, i));
+                @compileError("Missing key");
+            }
+        }
+    }
+}
+
 /// A "struct" with enum keys
 pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
     const fields = @typeInfo(Enum).Enum.fields;
@@ -11,35 +39,11 @@ pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
         const V = @This();
         data: [fieldCount]Value,
 
-        /// Example:
-        /// ```zig
-        /// EnumArray(enum{One, Two}, boolean)
-        ///   .init(.{.One = true, .Two = false});
-        /// ```
         pub fn init(values: var) V {
             const ValuesType = @TypeOf(values);
             const valuesInfo = @typeInfo(ValuesType).Struct;
-            comptime {
-                var ensure: [fieldCount]bool = undefined;
-                for (fields) |field, i| {
-                    ensure[i] = false;
-                }
-                for (valuesInfo.fields) |field| {
-                    // var val: Enum = std.meta.stringToEnum(Enum, field.name);
-                    var val: Enum = @field(Enum, field.name);
-                    const index = @enumToInt(val);
-                    if (ensure[index]) {
-                        @compileError("Duplicate key: " ++ field.name);
-                    }
-                    ensure[index] = true;
-                }
-                for (fields) |field, i| {
-                    if (!ensure[i]) {
-                        @compileLog(@intToEnum(Enum, i));
-                        @compileError("Missing key");
-                    }
-                }
-            }
+            ensureAllDefined(ValuesType, Enum);
+
             var res: [fieldCount]Value = undefined;
             inline for (valuesInfo.fields) |field| {
                 var val: Enum = @field(Enum, field.name);
@@ -48,12 +52,6 @@ pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
             }
             return .{ .data = res };
         }
-
-        /// Example:
-        /// ```zig
-        /// EnumArray(enum{One, Two}, boolean)
-        ///   .initDefault(false);
-        /// ```
         pub fn initDefault(value: Value) V {
             var res: [fieldCount]Value = undefined;
             inline for (fields) |field, i| {
@@ -62,21 +60,12 @@ pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
             return .{ .data = res };
         }
 
-        /// Example:
-        /// ```zig
-        /// var enumArr: EnumArray(enum{One, Two}, boolean)
-        /// enumArr.get(.One);
-        /// enumArr.get(.Two);
-        /// ```
         pub fn get(arr: V, key: Enum) Value {
             return arr.data[@enumToInt(key)];
         }
-
-        /// gets a mutable pointer
         pub fn getPtr(arr: *V, key: Enum) *Value {
             return &arr.data[@enumToInt(key)];
         }
-        /// sets the value and returns the previous value
         pub fn set(arr: *V, key: Enum, value: Value) Value {
             var prev = arr.data[@enumToInt(key)];
             arr.data[@enumToInt(key)] = value;
@@ -86,18 +75,81 @@ pub fn EnumArray(comptime Enum: type, comptime Value: type) type {
 }
 
 // holds all the values of the union at once instead of just one value
-pub fn UnionArray(comptime Union: T) type {
+pub fn UnionArray(comptime Union: type) type {
     const tinfo = @typeInfo(Union);
-    const Enum = tinfo.Union.tag_type;
+    const Enum = tinfo.Union.tag_type.?;
+    const Value = Union;
+
+    const fields = @typeInfo(Enum).Enum.fields;
+    const fieldCount = fields.len;
     return struct {
         const V = @This();
         data: [fieldCount]Value,
-        /// UnionArray(union{a: i32, b: SomeEnum})
-        /// .init(.{.a = 25, .b = enumvalue})
-        /// so internally they are stored as union values
-        /// but it doesn't look like it
-        pub fn init(values: var) V {
-            // needs to assemble the object
+
+        pub fn initUndefined() V {
+            var res: [fieldCount]Value = undefined;
+            return .{ .data = res };
+        }
+
+        pub fn get(arr: V, key: Enum) Value {
+            return arr.data[@enumToInt(key)];
+        }
+        pub fn getPtr(arr: *V, key: Enum) *Value {
+            return &arr.data[@enumToInt(key)];
+        }
+        pub fn set(arr: *V, value: Union) Value {
+            const enumk = @enumToInt(std.meta.activeTag(value));
+            defer arr.data[enumk] = value; // does this make the control flow more confusing? it's nice because I don't need a temporary name but it also could definitely be more confusing.
+            return arr.data[enumk];
         }
     };
+}
+
+test "enum array" {
+    const Enum = enum { One, Two, Three };
+    const EnumArr = EnumArray(Enum, bool);
+
+    var val = EnumArr.initDefault(false);
+    std.testing.expect(!val.get(.One));
+    std.testing.expect(!val.get(.Two));
+    std.testing.expect(!val.get(.Three));
+
+    _ = val.set(.Two, true);
+    _ = val.set(.Three, true);
+
+    std.testing.expect(!val.get(.One));
+    std.testing.expect(val.get(.Two));
+    std.testing.expect(val.get(.Three));
+}
+
+test "enum array initialization" {
+    const Enum = enum { One, Two, Three };
+    const EnumArr = EnumArray(Enum, bool);
+
+    var val = EnumArr.init(.{ .One = true, .Two = false, .Three = true });
+    std.testing.expect(val.get(.One));
+    std.testing.expect(!val.get(.Two));
+    std.testing.expect(val.get(.Three));
+}
+
+test "union array" {
+    const Union = union(enum) { One: bool, Two: []const u8, Three: i64 };
+    const UnionArr = UnionArray(Union);
+
+    // var val = UnionArr.init(.{ .One = true, .Two = "hi!", .Three = 27 });
+    var val = UnionArr.initUndefined();
+    _ = val.set(.{ .One = true });
+    _ = val.set(.{ .Two = "hi!" });
+    _ = val.set(.{ .Three = 27 });
+
+    std.testing.expect(val.get(.One).One);
+    std.testing.expect(std.mem.eql(u8, val.get(.Two).Two, "hi!"));
+    std.testing.expect(val.get(.Three).Three == 27);
+
+    _ = val.set(.{ .Two = "bye!" });
+    _ = val.set(.{ .Three = 54 });
+
+    std.testing.expect(val.get(.One).One);
+    std.testing.expect(std.mem.eql(u8, val.get(.Two).Two, "bye!"));
+    std.testing.expect(val.get(.Three).Three == 54);
 }
