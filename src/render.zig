a2c065ecc5ce4af3eb5c26feb75300a1a70355a3
pfg <omegariisthebest@gmail.com>
2020-05-08 03:29:26 -0700

    pub usingnamespace switch

--- src/render.zig
+++ src/render.zig
@@ -1,1 +1,6 @@
-pub usingnamespace @import("./renderers/sdl.zig");
+const renderer: enum { sdl, raylib } = .sdl; // should be passed in by build somehow but there is nothing in builtin that allows that
+
+pub usingnamespace switch (renderer) {
+    .sdl => @import("./renderers/sdl.zig"),
+    .raylib => @import("./renderers/raylib.zig"),
+};
