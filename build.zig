4dd885ec17d79825b4d85214305880c8ba9abf6a
pfg <omegariisthebest@gmail.com>
2020-05-08 19:42:44 -0700

    zig build test, zig build run

--- build.zig
+++ build.zig
@@ -1,23 +1,10 @@
 const std = @import("std");
 const Builder = std.build.Builder;
 
-pub fn build(b: *Builder) void {
-    const fmt = b.addFmt(&[_][]const u8{ "src", "build.zig" });
+pub const Renderer = enum { sdl, raylib };
 
-    const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("zigmd", "src/main.zig");
-    exe.setBuildMode(mode);
-
+pub fn addBuildOptions(b: *Builder, renderer: Renderer, mode: var, exe: var) void {
     exe.linkLibC();
-
-    const choice = b.option([]const u8, "render", "renderer <sdl|raylib>") orelse {
-        std.debug.warn("Expected -Drender=<sdl|raylib>\n", .{});
-        return;
-    };
-    const renderer = std.meta.stringToEnum(enum { sdl, raylib }, choice) orelse {
-        std.debug.warn("Must be <sdl|raylib>, got <{}>.", .{choice});
-        return;
-    };
 
     switch (renderer) {
         .sdl => {
@@ -48,13 +35,38 @@ pub fn build(b: *Builder) void {
     exe.addCSourceFile("deps/build/tree-sitter-markdown/src/parser.c", &[_][]const u8{});
     exe.addObjectFile("deps/build/tree-sitter-markdown/src/scanner.o");
     exe.addCSourceFile("deps/build/tree-sitter-json/src/parser.c", &[_][]const u8{});
+}
 
+pub fn build(b: *Builder) void {
+    const fmt = b.addFmt(&[_][]const u8{ "src", "build.zig" });
+
+    const choice = b.option([]const u8, "render", "renderer <sdl|raylib>") orelse "sdl";
+    const renderer = std.meta.stringToEnum(Renderer, choice) orelse {
+        std.debug.warn("Must be <sdl|raylib>, got <{}>.", .{choice});
+        return;
+    };
+
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("zigmd", "src/main.zig");
+    exe.setBuildMode(mode);
+    addBuildOptions(b, renderer, mode, exe);
     exe.install();
+
+    const testt = b.addTest("src/main.zig");
+    addBuildOptions(b, renderer, mode, testt);
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
-    fmt.step.dependOn(&run_cmd.step);
+
+    const fmt_step = b.step("fmt", "Format the code");
+    fmt_step.dependOn(&fmt.step);
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&fmt.step);
+    run_step.dependOn(&run_cmd.step);
+
+    const test_step = b.step("test", "Test the app");
+    test_step.dependOn(&fmt.step);
+    test_step.dependOn(&testt.step);
 }
