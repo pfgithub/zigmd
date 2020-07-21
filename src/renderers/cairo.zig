e35423c069f62bfd3b28d2cb911fb45dc1f6ca57
pfg <pfg@pfg.pw>
2020-07-20 20:30:58 -0700

    keypress wip

--- src/renderers/cairo.zig
+++ src/renderers/cairo.zig
@@ -7,7 +7,10 @@ const help = @import("../helpers.zig");
 pub usingnamespace @import("./common.zig");
 
 var nextEventFrame: ?anyframe = null;
-const RawEvent = struct { cr: *cairo_t };
+const RawEvent = union(enum) {
+    render: *cairo_t,
+    keypress: *GdkEventKey,
+};
 var eventResult: ?RawEvent = undefined;
 
 pub fn waitNextEvent() ?RawEvent {
@@ -18,22 +21,39 @@ pub fn waitNextEvent() ?RawEvent {
     return eventResult;
 }
 
-export fn zig_on_draw_event(widget: *GtkWidget, cr: *cairo_t) callconv(.C) void {
-    eventResult = .{ .cr = cr };
+export fn zig_on_draw_event(cr: *cairo_t) callconv(.C) void {
+    eventResult = .{ .render = cr };
+    resume nextEventFrame orelse @panic("no one is waiting for an event");
+}
+export fn zig_on_keypress_event(evk: *GdkEventKey) callconv(.C) void {
+    eventResult = .{ .keypress = evk };
     resume nextEventFrame orelse @panic("no one is waiting for an event");
 }
 
 pub fn asyncMain() void {
     while (waitNextEvent()) |ev| {
-        const cr = ev.cr;
-        cairo_set_source_rgb(cr, 0, 0, 0);
-        cairo_select_font_face(cr, "Sans", .CAIRO_FONT_SLANT_NORMAL, .CAIRO_FONT_WEIGHT_NORMAL);
-        cairo_set_font_size(cr, 40.0);
+        switch (ev) {
+            .render => |cr| {
+                cairo_set_source_rgb(cr, 0, 0, 0);
+                cairo_select_font_face(cr, "Sans", .CAIRO_FONT_SLANT_NORMAL, .CAIRO_FONT_WEIGHT_NORMAL); // you're supposed to use backend specific fns
+                cairo_set_font_size(cr, 40.0);
 
-        cairo_move_to(cr, 10.0, 50.0);
-        cairo_show_text(cr, "Cairo Test.");
+                cairo_move_to(cr, 10.0, 50.0);
+                cairo_show_text(cr, "Cairo Test."); // you're supposed to use cairo_show_glyphs instead of this.
+                // this is useless because it can't measure text
+                // so I need to use cairo_show_glyphs
+            },
+            .keypress => |kp| {
+                std.debug.warn("Keypress: {}\n", .{kp});
+            },
+        }
     }
 }
+// one of the reasons for this is I think buttons could look neat if they tilted and that would work better if there was a bit of gradient
+// would look neat
+// reminder to try that
+// mostly I want shadows and rounded rectangles though.
+// and sane, working text rendering
 
 pub fn main() !void {
     _ = async asyncMain(); // run until first suspend
--- src/renderers/cairo/cairo_cbind.h
+++ src/renderers/cairo/cairo_cbind.h
@@ -9,47 +9,54 @@
 	#define IMPLONLY(body)
 #endif
 
-IMPLONLY(void zig_on_draw_event(GtkWidget *widget, cairo_t *cr);)
+IMPLONLY(
 
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, 
-    gpointer user_data)
-IMPL({
-  zig_on_draw_event(widget, cr);
+void zig_on_draw_event(cairo_t *cr);
+void zig_on_keypress_event(GdkEventKey *widget);
 
-  return FALSE;
-})
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+	zig_on_draw_event(cr);
+	return FALSE;
+}
 
-static void destroy( GtkWidget *widget,
-                     gpointer   data )
-{
+static gboolean on_keypress_event(GtkWidget *widget, GdkEventKey *event, gpointer data) {
+	zig_on_keypress_event(event);
+	return TRUE; // = stop propagation. ok as long as we don't use any gtk widgets
+}
+
+static void destroy(GtkWidget *widget, gpointer data) {
     gtk_main_quit ();
 }
 
+)
+
+// maybe switch to gdk instead? give up on gtk?
 
 int start_gtk(int argc, char *argv[])
 IMPL({
-  GtkWidget *window;
-  GtkWidget *darea;
+	GtkWidget *window;
+	GtkWidget *darea;
 
-  gtk_init(&argc, &argv);
+	gtk_init(&argc, &argv);
 
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
-  darea = gtk_drawing_area_new();
-  gtk_container_add(GTK_CONTAINER(window), darea);
+	gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
 
-  g_signal_connect(G_OBJECT(darea), "draw", 
-      G_CALLBACK(on_draw_event), NULL); 
-  g_signal_connect(window, "destroy",
-      G_CALLBACK(destroy), NULL);
+	darea = gtk_drawing_area_new();
+	gtk_container_add(GTK_CONTAINER(window), darea);
 
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_default_size(GTK_WINDOW(window), 400, 90); 
-  gtk_window_set_title(GTK_WINDOW(window), "GTK window");
+	g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(on_draw_event), NULL); 
+	g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
+	g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(on_keypress_event), NULL);
 
-  gtk_widget_show_all(window);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	gtk_window_set_default_size(GTK_WINDOW(window), 400, 90); 
+	gtk_window_set_title(GTK_WINDOW(window), "GTK window");
 
-  gtk_main();
+	gtk_widget_show_all(window);
 
-  return 0;
+	gtk_main();
+
+	return 0;
 })
